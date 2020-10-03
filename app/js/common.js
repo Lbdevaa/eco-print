2bac112c167065b04473458dce3b6fb2d8ec9e6a
Merak <mesheryakov97@gmail.com>
2020-10-03 13:05:10 +0500

    $feat: create main page pc

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,36 @@
-$(function() {
+$(function () {
+  // input mask phone
+  $("input[name*='phone']").inputmask("+7 (999) 999-99-99");
 
-	// Custom JS
+  //burger
+  $(".burger").click(function () {
+    $(this).toggleClass("active");
+    $(".burger-menu").fadeToggle(200);
+  });
+  // свернуть меню при клике
+  $(".burger-menu__url").click(function () {
+    $(".burger-menu").fadeToggle(200);
+    $(".burger").toggleClass("active");
+  });
+  //popup
+  $(".open-popup").magnificPopup({
+    type: "inline",
+    midClick: true, // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+  });
 
+  //
+  // const anchors = document.querySelectorAll('a[href*="#"]');
+
+  // for (let anchor of anchors) {
+  //   anchor.addEventListener("click", function (e) {
+  //     e.preventDefault();
+
+  //     const blockID = anchor.getAttribute("href").substr(1);
+
+  //     document.getElementById(blockID).scrollIntoView({
+  //       behavior: "smooth",
+  //       block: "start",
+  //     });
+  //   });
+  // }
 });
---
+++ app/mail.php
@@ -0,0 +1,36 @@
+<?php 
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+$name = $_POST['name'];
+$phone = $_POST['phone']; 
+$email = $_POST['email']; 
+$description = $_POST['description'];
+$amount = $_POST['amount'];
+$interest = $_POST['interest'];
+$comment = $_POST['comment'];
+$nameForm = $_POST['nameForm'];
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.yandex.ru';  																							// Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'info@invite-practice.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = '*G91hyht'; // Ваш пароль от почты с которой будут отправляться письма  
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
+
+$mail->setFrom('info@invite-practice.ru'); // от кого будет уходить письмо?  
+$mail->addAddress('info@invite-practice.ru');     // Кому будет уходить письмо   
+$mail->isHTML(true);                                  // Set email format to HTML
+   
+
+// $mail->Body    = '<b>Имя:</b> ' . $name .  '<br><b>Телефон:</b> ' . $phone . '<br><b>Email:</b> ' . $email . '<br> Форма: ' . $nameForm;
+$mail->Body    = '<b>Имя:</b> ' . $name .  '<br><b>Телефон:</b> ' . $phone . '<br> Форма заявки: ' . $nameForm .'<br>Способ оплаты:</br> ' . $description . '<br>Стоимость: </br> ' . $amount . '<br><b>Email:</b> ' . $email .'<br>Интересует: </br>' . $interest  . '<br>Комментарий: </br>' . $comment;
+$mail->Subject = 'Заявка с сайта Invite Practice';
+$mail->AltBody = '';
+$mail->send();
+?>
+ 
+ 
